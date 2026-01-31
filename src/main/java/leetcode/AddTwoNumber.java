6890757f1c9c1a2efba030d7a68f9c3a4a64b6f6
Justin Hsu <justinhsu@soetek.com.tw>
2026-01-31 15:56:51 +0800

    add. add two numbers

---
+++ src/main/java/leetcode/AddTwoNumber.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+public class AddTwoNumber {
+    public ListNode addtwo(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0); // 答案白紙
+        ListNode curr = dummyHead; // 當下位置
+        int carry = 0; // 紀錄進位
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int x = (l1 != null) ? l1.value : 0;
+            int y = (l2 != null) ? l2.value : 0;
+            int sum = x + y + carry; // x+y+要記得加上進位值
+
+            carry = sum / 10; // 計算進位值
+
+            curr.next = new ListNode(sum % 10); // 填答案
+
+            curr = curr.next; // 換下一列
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        return dummyHead.next;
+    }
+}
