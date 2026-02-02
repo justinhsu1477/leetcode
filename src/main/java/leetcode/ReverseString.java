9459755fd28e4f1d738ba6250422a96657513c96
Justin Hsu <justinhsu@soetek.com.tw>
2026-02-02 08:56:52 +0800

    add. 反轉字串

---
+++ src/main/java/leetcode/ReverseString.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class ReverseString {
+    public  String reverseString(String s) {
+        int left = 0;
+        int right = s.length()-1;
+        char[] arr = s.toCharArray();
+        while (left < right) {
+            char tempt  = arr[left];
+            arr[left] = arr[right];
+            arr[right] = tempt;
+            left++;
+            right--;
+        }
+        return  String.valueOf(arr);
+    }
+}
